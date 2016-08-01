28a77ffd4e61ececae0a30d347115186b26464ee
Rob Wittman <rob@ihsdigital.com>
2016-08-01 11:46:18 -0400

    Added bash script

--- bin/gen_env.sh
+++ bin/gen_env.sh
@@ -7,6 +7,7 @@ function gen_string() {
 }
 
 FILENAME='/opt/.env'
+set -a
 API_KEY=$(gen_string)
 API_SECRET=$(gen_string)
 DB_PASS=$(gen_string)
@@ -16,6 +17,7 @@ DB_PORT=27017
 DB_NAME='notifications'
 WS_PORT=3000
 HTTP_PORT=8080
+set +a
 
 echo "API_KEY=${API_KEY}"       >> ${FILENAME}
 echo "API_SECRET=${API_SECRET}" >> ${FILENAME}
@@ -24,5 +26,3 @@ echo "DB_USER=${DB_USER}"       >> ${FILENAME}
 echo "DB_PASS=${DB_PASS}"       >> ${FILENAME}
 echo "DB_PORT=${DB_PORT}"       >> ${FILENAME}
 echo "DB_NAME=${DB_NAME}"       >> ${FILENAME}
-
-if
